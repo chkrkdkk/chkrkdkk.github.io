ac17a0cd85b4c10973fcea4339e7bf6112f5e749
chkrkdkk <50568179+chkrkdkk@users.noreply.github.com>
2019-05-14 10:33:58 +0900

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -15,8 +15,8 @@
  // Note that you can only use Firebase Messaging here, other Firebase libraries
  // are not available in the service worker.
  **/
- importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
- importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+ importScripts('https://www.gstatic.com/firebasejs/5.8.6/firebase-app.js');
+ importScripts('https://www.gstatic.com/firebasejs/5.8.6/firebase-messaging.js');
 
  // Initialize the Firebase app in the service worker by passing in the
  // messagingSenderId.
